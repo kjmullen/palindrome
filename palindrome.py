e94a2fa6fddad0324e5bc074ecec69ca3103c976
Kevin Mullen <kevinkozzik@gmail.com>
2016-03-16 14:46:27 -0700

    normal mode complete

--- palindrome.py
+++ palindrome.py
@@ -1,2 +1,16 @@
+import re
+
+
+def subbing(arg):
+    return re.sub(r'\W+', "", arg)
+
+
 def palindrome(string):
-    return False
+    string = string.lower()
+    string = subbing(string)
+    if len(string) == 0 or len(string) == 1:
+        return True
+    elif string[0] != string[len(string)-1]:
+        return False
+    else:
+        return palindrome(string[1:len(string)-1])
